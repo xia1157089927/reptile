3516af4919fb3f6184c8977bdd5bc4ef75605d56
Xiams <1157089927@qq.com>
2017-04-17 16:25:42 +0800

    权限 拦截器

---
+++ src/main/java/com/xia/reptile/config/PrivilegeInterceptor.java
@@ -0,0 +1,17 @@
+package com.xia.reptile.config;
+
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+
+/**
+ * 执行方法拦截器
+ * @author xiams
+ */
+public class PrivilegeInterceptor implements MethodInterceptor{
+	
+	public Object invoke(MethodInvocation invocation) throws Throwable {
+		
+		return null;
+	}
+	
+}
