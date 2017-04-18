dac9cf89397a27e0d3b2e98bbb2959159800f91b
Xiams <1157089927@qq.com>
2017-04-18 13:49:26 +0800

    添加时间定时器

--- src/main/java/com/xia/reptile/Application.java
+++ src/main/java/com/xia/reptile/Application.java
@@ -7,12 +7,41 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling //定时器
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+		
+		int a = 0;
+		Integer aa = a;
+		Integer ab = a;
+		System.out.println(aa == ab);
+		System.out.println(aa.equals(ab));
+		
+		Integer ac = 127;
+		Integer ad = 127;
+		System.out.println(ac.equals(ad));
+		System.out.println(ac == ad);
+		
+		Integer ae = 128;
+		Integer af = 128;
+		System.out.println(ae.equals(af));
+		System.out.println(ae == af);
+		
+		Integer ag = -128;
+		Integer ah = -128;
+		System.out.println(ag.equals(ah));
+		System.out.println(ag == ah);
+		
+		Integer ai = -129;
+		Integer aj = -129;
+		System.out.println(ai.equals(aj));
+		System.out.println(ai == aj);
+		
 	}
 	@Bean
 	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
